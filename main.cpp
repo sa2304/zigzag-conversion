59813e0598ea715db01fd15a7bbc21b42f9de60d
Антон Шмидт <schmidt.ajax@gmail.com>
2022-01-13 19:47:15 +0700

    WIP: readTeeth

--- main.cpp
+++ main.cpp
@@ -10,7 +10,31 @@ class Solution {
     // FIXME
     return s;
   }
+
+  string readTeeth(string_view s, int numRows) {
+    string result;
+    if (0 < numRows) {
+      int left = 0;
+      int right = numRows * 2 - 2;
+      while (left < right) {
+        result.push_back(s[left++]);
+        result.push_back(s[right--]);
+      }
+      result.push_back(s[left]);
+    }
+
+    return result;
+  }
 };
+
+void TestReadTeeth() {
+  Solution s;
+  assert("0A192837465"s == s.readTeeth("0123456789A"s, 6));
+  assert("0615243"s == s.readTeeth("0123456789A"s, 4));
+  assert("04132"s == s.readTeeth("0123456789A"s, 3));
+  assert("021"s == s.readTeeth("0123456789A"s, 2));
+  assert("0"s == s.readTeeth("0123456789A"s, 1));
+}
 
 void TestConvert() {
   Solution s;
@@ -20,6 +44,7 @@ void TestConvert() {
 }
 
 int main() {
+  TestReadTeeth();
   TestConvert();
   std::cout << "Ok!" << std::endl;
   return 0;
